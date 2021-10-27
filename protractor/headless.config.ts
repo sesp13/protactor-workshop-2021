3fbb5d11d55bc872ea2c8a77a8cb43d335636d71
sesp13 <sespinosarte99@gmail.com>
2021-10-26 22:58:16 -0500

    remove selenium and add async await to google spec

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -4,6 +4,12 @@ import { reporter } from "./helpers/reporter";
 export const config: Config = {
   framework: "jasmine",
   specs: ["../test/google.spec.js"],
+  capabilities: {
+    browserName: "chrome",
+    chromeOptions: {
+      args: ["--headless", "--disable-gpu"],
+    },
+  },
   SELENIUM_PROMISE_MANAGER: false,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
---
+++ protractor/helpers/reporter.ts
@@ -0,0 +1,10 @@
+import { SpecReporter, StacktraceOption } from "jasmine-spec-reporter";
+export let reporter = () => {
+  jasmine.getEnv().addReporter(
+    new SpecReporter({
+      spec: {
+        displayStacktrace: StacktraceOption.PRETTY,
+      },
+    })
+  );
+};
