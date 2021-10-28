e65d2da1ef94fae0b8ee96fdae1b81f20c9b9a83
sesp13 <sespinosarte99@gmail.com>
2021-10-27 20:35:55 -0500

    add gloabl implicit waittimes to the tests

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -26,28 +26,17 @@ describe('Buy a t-shirt', () => {
 
   it('then should be bought a t-shirt', async () => {
     await browser.get('http://automationpractice.com/');
-    await browser.sleep(3000);
     await menuContentPage.goToTShirtMenu();
-    await browser.sleep(3000);
     await productListPage.addToCart();
-    await browser.sleep(3000);
     await productAddedModalPage.proceedToCheckOut();
-    await browser.sleep(3000);
     await summaryStepPage.proceedToCheckOut();
-    await browser.sleep(3000);
     await signInStepPage.fillForm('aperdomobo@gmail.com', 'WorkshopProtractor');
     await signInStepPage.signIn();
-    await browser.sleep(3000);
     await addressStepPage.proceedToCheckOut();
-    await browser.sleep(3000);
     await shippingStepPage.checkTermsButton();
-    await browser.sleep(3000);
     await shippingStepPage.proceedToCheckOut();
-    await browser.sleep(3000);
     await paymentStepPage.payByCheck();
-    await browser.sleep(3000);
     await bankPaymentStepPage.confirmPayment();
-    await browser.sleep(3000);
 
     await expect(orderSummaryStepPage.getConfirmationMessage()).toBe(
       'Your order on My Store is complete.',
